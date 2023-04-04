fc585e5fbb1ebe38a9ba92aa96bcc39c8ca7d2fa
Harrison Hdez <hhdezprogrammer@gmail.com>
2023-04-04 15:39:15 -0600

    commit

--- src/Components/Location.jsx
+++ src/Components/Location.jsx
@@ -3,16 +3,22 @@ import React from 'react'
 export const Location = () => {
   return (
     <section className="location">
-         <p className="location__paragraph">
-             Ubicados en los vecindarios más populares 
-             de CDMX La Condesa & Col. del Valle, zonas 
-             repletas de COLORES + CULTURA & llenas de 
-             entretenimiento. MY SUITES BY LA CONDESA 
-             son unas sofisticadas residencias AMUEBLADAS con  
-             estilo MODERNO que combinan con lo clásico 
-             de la ciudad donde sus detalles artísticos le dan 
-             un toque cálido y un distintivo  ÚNICO.
-         </p>
+        <div className="location__paragraph-container">
+            <p className="location__paragraph">
+                Ubicados en los vecindarios más populares de CDMX La Condesa & Col. del Valle, zonas repletas de COLORES + CULTURA & llenas de entretenimiento. MY SUITES BY LA CONDESA son unas sofisticadas residencias AMUEBLADAS con estilo MODERNO que combinan con lo clásico de la ciudad donde sus detalles artísticos le dan un toque cálido y un distintivo  ÚNICO.
+            </p>
+        </div>
+        <div className="location__images-container">
+            <div className="location__img">
+                <img src="/images/image-location-1.jpg" alt="img" />
+                    <p><span>La Condesa,</span> su estilo art déco le da un ambiente europeo.</p>
+            </div>
+            <div className="location__img">
+                    <img src="/images/image-location-2.jpg" alt="img" />
+                    <p><span>Col. del Valle,</span> barrio céntrico, tranquilo y muy bien conectado.</p> 
+            </div>
+        </div>
     </section>
+
   )
 }
